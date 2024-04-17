0844c034d0be633423426ecadecc8742a941e9aa
Sabrun <officialsabrun@gmail.com>
2024-04-17 02:05:20 -0400

    Updated files

---
+++ src/components/PreLoader/PreLoader.tsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import "./PreLoader.css";
+
+const PreLoader = ({ show }: { show: boolean }) => {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setLoading(show);
+  }, [show]);
+
+  return (
+    <div className={`preloader ${loading ? "show " : "hide"}`}>
+      <img src="/Pokepedia.png" alt="Pokepedia Logo" />
+    </div>
+  );
+};
+
+export default PreLoader;
